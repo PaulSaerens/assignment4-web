161f1b2b094e3e832e5175c4cbcd4a88afa8c18b
Paul Saerens <paul.saerens@epitech.eu>
2022-05-13 17:37:02 +0100

    feat: add & delete usr

--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -11,17 +11,110 @@ export class WorkspaceComponent implements OnInit {
 
     @Input() workspace: any;
 
+    public newCollab: string = ''
+    public collabs: any = []
+
+    private usr: any = []
+
     constructor(private http: HttpClient, private cookieService: CookieService) { }
 
     ngOnInit(): void {
+        this.getAllUsers()
+        this.getUsers()
     }
+
+    getAllUsers() {
+        const options = {
+            params: new HttpParams(),
+            headers: new HttpHeaders({
+                "Content-Type": "application/json",
+                Authorization: this.cookieService.get('token')
+            }),
+        }
+        this.http
+            .get(`https://tasklist-griffith.herokuapp.com/users/all`, options)
+            .subscribe(res => {
+                const r = res as any;
+                for (let user of r.content.users) {
+                    this.usr.push({
+                        id: user._id,
+                        name: user.username
+                    })
+                }
+            });
+    }
+
+    nameToId() {
+        for (const user of this.usr) {
+            if (this.newCollab == user.name) {
+                return user.id
+            }
+        }
+        return ''
+    }
+
 
     addCollab() {
-
+        const options = {
+            params: new HttpParams(),
+            headers: new HttpHeaders({
+                "Content-Type": "application/json",
+                Authorization: this.cookieService.get('token')
+            }),
+        }
+        const body = {
+            "collaboratorId": this.nameToId()
+        }
+        this.http
+            .post(`https://tasklist-griffith.herokuapp.com/workspaces/${this.workspace._id}/addCollaborator`, body, options)
+            .subscribe(res => {
+                this.updateWorkspace()
+            });
     }
 
-    delCollab() {
+    delCollab(id: any) {
+        const options = {
+            params: new HttpParams(),
+            headers: new HttpHeaders({
+                "Content-Type": "application/json",
+                Authorization: this.cookieService.get('token')
+            }),
+        }
+        this.http
+            .delete(
+                `https://tasklist-griffith.herokuapp.com/workspaces/${this.workspace._id}/removeCollaborator/${id}`, options
+            )
+            .subscribe(res => {
+                this.updateWorkspace()
+            });
+    }
 
+    getUsers() {
+        this.collabs = []
+        const options = {
+            params: new HttpParams(),
+            headers: new HttpHeaders({
+                "Content-Type": "application/json",
+                Authorization: this.cookieService.get('token')
+            }),
+        }
+        for (let col of this.workspace.collaborators) {
+            console.log(col)
+            this.http
+                .get(`https://tasklist-griffith.herokuapp.com/users/${col}`, options)
+                .subscribe(res => {
+                    console.log(res)
+                    let r = res as any;
+                    this.collabs.push({
+                        id: r.content.user._id,
+                        name: r.content.user.username
+                    })
+                });
+        }
+    }
+
+    updateWorkspace() {
+        window.location.reload()
     }
 
     delWorkspace() {
