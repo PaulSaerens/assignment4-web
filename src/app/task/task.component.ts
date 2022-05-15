6421d3707bc16a0394045c8e707c5d5399c374dc
Julien <julien.rondin@orange.fr>
2022-05-15 14:35:48 +0100

    .

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,72 +1,79 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Component, Input, OnInit } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {Component, Input, OnInit} from '@angular/core';
+import {CookieService} from 'ngx-cookie-service';
 
 @Component({
-    selector: 'app-task',
-    templateUrl: './task.component.html',
-    styleUrls: ['./task.component.scss']
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.scss']
 })
 export class TaskComponent implements OnInit {
-    @Input() taskId: any;
-    @Input() workspace: any;
+  @Input() taskId: any;
+  @Input() workspace: any;
 
-    public task: any = {}
+  public task: any = {}
 
-    constructor(private http: HttpClient, private cookieService: CookieService) { }
+  constructor(private http: HttpClient, private cookieService: CookieService) {
+  }
 
-    ngOnInit(): void {
-        this.task = this.getTask();
+  ngOnInit(): void {
+    this.task = this.getTask();
+  }
+
+  getTask() {
+    const options = {
+      params: new HttpParams(),
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        Authorization: this.cookieService.get('token')
+      }),
     }
+    this.http
+      .get(`https://tasklist-griffith.herokuapp.com/tasks/${this.taskId}`, options)
+      .subscribe(res => {
+        const r = res as any
+        this.task = r.content.task
+        console.log(this.task)
+      });
+  }
 
-    getTask() {
-        const options = {
-            params: new HttpParams(),
-            headers: new HttpHeaders({
-                "Content-Type": "application/json",
-                Authorization: this.cookieService.get('token')
-            }),
-        }
-        this.http
-            .get(`https://tasklist-griffith.herokuapp.com/tasks/${this.taskId}`, options)
-            .subscribe(res => {
-                const r = res as any
-                this.task = r.content.task
-                console.log(this.task)
-            });
+  check() {
+    const options = {
+      params: new HttpParams(),
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        Authorization: this.cookieService.get('token')
+      }),
     }
+    const body = {}
+    console.log("aaaaaaaaa");
+    this.http
+      .post(`https://tasklist-griffith.herokuapp.com/tasks/${this.taskId}/complete`, body, options)
+      .subscribe(res => {
+        let r = res as any
+        console.log(res);
+        this.task.completed = r.content.task.completed;
+      });
+  }
 
-    check() {
-        const options = {
-            params: new HttpParams(),
-            headers: new HttpHeaders({
-                "Content-Type": "application/json",
-                Authorization: this.cookieService.get('token')
-            }),
-        }
-        const body = {}
-        this.http
-            .post(`https://tasklist-griffith.herokuapp.com/tasks/${this.taskId}/complete`, body, options)
-            .subscribe(res => {
-                this.task.completed = true;
-            });
+  deleteTask() {
+    const options = {
+      params: new HttpParams(),
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        Authorization: this.cookieService.get('token')
+      }),
     }
+    this.http
+      .delete(
+        `https://tasklist-griffith.herokuapp.com/workspaces/${this.workspace._id}/deleteTask/${this.taskId}`, options
+      )
+      .subscribe(res => {
+        let r = res as any;
+        if ('workspace' in r.content)
+          this.workspace.tasks = r.content.workspace.tasks
+//        window.location.reload()
+      });
 
-    deleteTask() {
-        const options = {
-            params: new HttpParams(),
-            headers: new HttpHeaders({
-                "Content-Type": "application/json",
-                Authorization: this.cookieService.get('token')
-            }),
-        }
-        this.http
-            .delete(
-                `https://tasklist-griffith.herokuapp.com/workspaces/${this.workspace}/deleteTask/${this.taskId}`, options
-            )
-            .subscribe(res => {
-                window.location.reload()
-            });
-
-    }
+  }
 }
--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -1,168 +1,185 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Component, Input, OnInit } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {Component, Input, OnInit} from '@angular/core';
+import {CookieService} from 'ngx-cookie-service';
 
 @Component({
-    selector: 'app-workspace',
-    templateUrl: './workspace.component.html',
-    styleUrls: ['./workspace.component.scss']
+  selector: 'app-workspace',
+  templateUrl: './workspace.component.html',
+  styleUrls: ['./workspace.component.scss']
 })
 export class WorkspaceComponent implements OnInit {
 
-    @Input() workspace: any;
+  @Input() workspace: any;
 
-    public newCollab: string = ''
-    public newTask: string = ''
-    public collabs: any = []
-    public isHidden: boolean = true;
-    public selectedObject = "MEDIUM"
-    public selection = ["HIGH", "MEDIUM", "LOW"]
-    public taskDesc: string = ''
+  public newCollab: string = ''
+  public newTask: string = ''
+  public collabs: any = []
+  public isHidden: boolean = false;
+  public selectedObject = "MEDIUM"
+  public selection = ["HIGH", "MEDIUM", "LOW"]
+  public taskDesc: string = ''
 
-    private usr: any = []
+  private usr: any = []
 
-    constructor(private http: HttpClient, private cookieService: CookieService) { }
+  constructor(private http: HttpClient, private cookieService: CookieService) {
+  }
 
-    ngOnInit(): void {
-        this.isHidden = true
-        this.getAllUsers()
-        this.getUsers()
+  ngOnInit(): void {
+    this.isHidden = false
+    this.getAllUsers()
+    this.getUsers()
+  }
+
+
+  hideCollaborators() {
+    this.isHidden = !this.isHidden
+    console.log(this.isHidden);
+  }
+
+  addTask() {
+    const options = {
+      params: new HttpParams(),
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        Authorization: this.cookieService.get('token')
+      }),
     }
-
-
-    hideCollaborators() {
-        this.isHidden = !this.isHidden
+    const body = {
+      "name": this.newTask,
+      "description": this.taskDesc,
+      "assignedTo": [this.cookieService.get('id')],
+      "completed": false,
+      "priority": this.selectedObject
     }
+    this.http
+      .post(`https://tasklist-griffith.herokuapp.com/workspaces/${this.workspace._id}/newTask`, body, options)
+      .subscribe(res => {
+        let r = res as any
+        if ('workspace' in r.content) {
+          this.workspace.tasks = r.content.workspace.tasks
+        }
+        console.log(this.workspace);
+      });
+  }
 
-    addTask() {
-        const options = {
-            params: new HttpParams(),
-            headers: new HttpHeaders({
-                "Content-Type": "application/json",
-                Authorization: this.cookieService.get('token')
-            }),
-        }
-        const body = {
-            "name": this.newTask,
-            "description": this.taskDesc,
-            "assignedTo": [this.cookieService.get('id')],
-            "completed": false,
-            "priority": this.selectedObject
-        }
-        this.http
-            .post(`https://tasklist-griffith.herokuapp.com/workspaces/${this.workspace._id}/newTask`, body, options)
-            .subscribe(res => {
-                this.updateWorkspace()
-            });
+
+  getAllUsers() {
+    const options = {
+      params: new HttpParams(),
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        Authorization: this.cookieService.get('token')
+      }),
     }
-
-
-
-    getAllUsers() {
-        const options = {
-            params: new HttpParams(),
-            headers: new HttpHeaders({
-                "Content-Type": "application/json",
-                Authorization: this.cookieService.get('token')
-            }),
+    this.http
+      .get(`https://tasklist-griffith.herokuapp.com/users/all`, options)
+      .subscribe(res => {
+        const r = res as any;
+        for (let user of r.content.users) {
+          this.usr.push({
+            id: user._id,
+            name: user.username
+          })
         }
-        this.http
-            .get(`https://tasklist-griffith.herokuapp.com/users/all`, options)
-            .subscribe(res => {
-                const r = res as any;
-                for (let user of r.content.users) {
-                    this.usr.push({
-                        id: user._id,
-                        name: user.username
-                    })
-                }
-            });
+      });
+  }
+
+  nameToId() {
+    for (const user of this.usr) {
+      if (this.newCollab == user.name) {
+        return user.id
+      }
     }
+    return ''
+  }
 
-    nameToId() {
-        for (const user of this.usr) {
-            if (this.newCollab == user.name) {
-                return user.id
-            }
-        }
-        return ''
+
+  addCollab() {
+    const options = {
+      params: new HttpParams(),
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        Authorization: this.cookieService.get('token')
+      }),
     }
-
-
-    addCollab() {
-        const options = {
-            params: new HttpParams(),
-            headers: new HttpHeaders({
-                "Content-Type": "application/json",
-                Authorization: this.cookieService.get('token')
-            }),
-        }
-        const body = {
-            "collaboratorId": this.nameToId()
-        }
-        this.http
-            .post(`https://tasklist-griffith.herokuapp.com/workspaces/${this.workspace._id}/addCollaborator`, body, options)
-            .subscribe(res => {
-                this.updateWorkspace()
-            });
+    const body = {
+      "collaboratorId": this.nameToId()
     }
-
-    delCollab(id: any) {
-        const options = {
-            params: new HttpParams(),
-            headers: new HttpHeaders({
-                "Content-Type": "application/json",
-                Authorization: this.cookieService.get('token')
-            }),
+    this.http
+      .post(`https://tasklist-griffith.herokuapp.com/workspaces/${this.workspace._id}/addCollaborator`, body, options)
+      .subscribe(res => {
+        let r = res as any;
+        console.log(this.workspace);
+        if ('workspace' in r.content) {
+          this.collabs = r.content.workspace.collaborators;
+          this.workspace.collaborators = r.content.workspace.collaborators;
+          this.getUsers()
         }
-        this.http
-            .delete(
-                `https://tasklist-griffith.herokuapp.com/workspaces/${this.workspace._id}/removeCollaborator/${id}`, options
-            )
-            .subscribe(res => {
-                this.updateWorkspace()
-            });
+      });
+  }
+
+  delCollab(id: any) {
+    const options = {
+      params: new HttpParams(),
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        Authorization: this.cookieService.get('token')
+      }),
     }
+    this.http
+      .delete(
+        `http://localhost:3000/workspaces/${this.workspace._id}/removeCollaborator/${id}`, options
+      )
+      .subscribe(res => {
+        let r = res as any
+        console.log(res)
+        if (r.content.workspace) {
+          this.collabs = r.content.workspace.collaborators
+          this.workspace.collaborators = r.content.workspace.collaborators
+          this.getUsers()
+        }
+      });
+  }
 
-    getUsers() {
-        this.collabs = []
-        const options = {
-            params: new HttpParams(),
-            headers: new HttpHeaders({
-                "Content-Type": "application/json",
-                Authorization: this.cookieService.get('token')
-            }),
-        }
-        for (let col of this.workspace.collaborators) {
-            this.http
-                .get(`https://tasklist-griffith.herokuapp.com/users/${col}`, options)
-                .subscribe(res => {
-                    let r = res as any;
-                    this.collabs.push({
-                        id: r.content.user._id,
-                        name: r.content.user.username
-                    })
-                });
-        }
+  getUsers() {
+    this.collabs = []
+    const options = {
+      params: new HttpParams(),
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        Authorization: this.cookieService.get('token')
+      }),
     }
+    for (let col of this.workspace.collaborators) {
+      this.http
+        .get(`https://tasklist-griffith.herokuapp.com/users/${col}`, options)
+        .subscribe(res => {
+          let r = res as any;
+          this.collabs.push({
+            id: r.content.user._id,
+            name: r.content.user.username
+          })
+        });
+    }
+  }
 
-    updateWorkspace() {
+  updateWorkspace() {
+    window.location.reload()
+  }
+
+  delWorkspace() {
+    const options = {
+      params: new HttpParams(),
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        Authorization: this.cookieService.get('token')
+      }),
+    }
+    this.http
+      .delete(`https://tasklist-griffith.herokuapp.com/workspaces/${this.workspace._id}`, options)
+      .subscribe(res => {
         window.location.reload()
-    }
-
-    delWorkspace() {
-        const options = {
-            params: new HttpParams(),
-            headers: new HttpHeaders({
-                "Content-Type": "application/json",
-                Authorization: this.cookieService.get('token')
-            }),
-        }
-        this.http
-            .delete(`https://tasklist-griffith.herokuapp.com/workspaces/${this.workspace._id}`, options)
-            .subscribe(res => {
-                this.updateWorkspace()
-            });
-    }
+      });
+  }
 
 }
